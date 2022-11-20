9bb405d42c4d0129c61f88fcac689f2fa50c6fb4
Sonya0932 <108592540+Sonya0932@users.noreply.github.com>
2022-11-20 15:59:28 +0200

    create task2,3

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,35 @@
+var throttle = require('lodash.throttle');
 
+const formEL = document.querySelector('.feedback-form');
+const inputEl = document.querySelector('.feedback-form input');
+const textareaEl = document.querySelector('.feedback-form textarea')
+const LOCALSTORAGE_KEY = 'feedback-form-state';
+
+formEL.addEventListener('input', throttle(evt => {
+    console.log(evt)
+    const objectToSave = { email: email.value, message: message.value };
+    localStorage.setItem(LOCALSTORAGE_KEY, JSON.stringify(objectToSave));
+  }, 500)
+);
+
+formEL.addEventListener('submit', evt => {
+    evt.preventDefault();
+    console.log({ email: email.value, message: message.value });
+    form.reset();
+    localStorage.removeItem(LOCALSTORAGE_KEY);
+  });
+  
+  const load = key => {
+    try {
+      const serializedState = localStorage.getItem(key);
+      return serializedState === null ? undefined : JSON.parse(serializedState);
+    } catch (error) {
+      console.error('Get state error: ', error.message);
+    }
+  };
+  
+  const storageData = load(LOCALSTORAGE_KEY);
+  if (storageData) {
+    email.value = storageData.email;
+    message.value = storageData.message;
+  }
